83a57af935551453c9bb00e04b0b728859c14d6f
Jason Lust <jasonlust@gmail.com>
2013-05-10 17:22:41 -0700

    Fix nodes from cleaning up during a re-add

--- cocos2dx/base_nodes/CCNode.cpp
+++ cocos2dx/base_nodes/CCNode.cpp
@@ -582,7 +582,7 @@ void CCNode::addChild(CCNode *child, int zOrder, int tag)
     CCAssert( child != NULL, "Argument must be non-nil");
     if (child->m_pParent != NULL) {
         child->retain();
-        child->removeFromParent();
+        child->removeFromParentAndCleanup(false);
         child->autorelease();
     }
     //CCAssert( child->m_pParent == NULL, "child already added. It can't be added again");
